fad21fa5cbadf41b4943d72e62272e4db92a6466
Teresia <teresia.schullstrom@valtech.se>
2016-09-03 17:40:32 +0200

    Lagt till context

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,34 @@
+# coding: utf-8
 from django.shortcuts import render
+
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
 
 # Create your views here.
 def startpage(request):
-	return render(request, "quiz/index.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/index.html", context)
 
-def quiz(request):
-	return render(request, "quiz/quizpage.html")
+def quiz(request, slug):
+	context = {
+		"quiz": quizzes[slug],
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/quizpage.html", context)
 
 def question(request):
 	return render(request, "quiz/questionpage.html")
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.startpage),
-	url(r"^quiz/[a-z-]+/$", views.quiz),
+	url(r"^quiz/([a-z-]+)/$", views.quiz),
 	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
 	url(r"^quiz/[a-z-]+/completed/$", views.completed),
 ]
